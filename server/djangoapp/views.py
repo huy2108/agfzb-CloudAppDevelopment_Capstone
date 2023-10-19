8ae97b6ed4da55b60dd63e5ab278595e3d6c12d1
Le Quang Huy <lequanghuy2108@gmail.com>
2023-10-19 01:20:56 -0400

    Changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -30,16 +30,51 @@ def contact(request):
     return render(request,'djangoapp/contact_us.html')
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    if (request.method == "POST"):
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('djangoapp:index')
+        else:
+            logger.debug('This username is not existed')
+            return render(request,'djangoapp/index.html')
+    else:
+        return render(request,'djangoapp/index.html')
+
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    
+    logout(request)
+    return redirect('djangoapp:index')
+
+
+def signup(request):
+    return render(request,'djangoapp/registration.html')
+
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    context = {}
+    if request.method == "POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        firstname=request.POST['firstname']
+        lastname=request.POST['lastname']
+        exist = False
+        try:
+            user= User.objects.get(username=username)
+            exist = True
+        except:
+            logger.debug("{} not exist".format(username))
+
+        if not exist:
+            user= User.objects.create_user(username=username,password=password,first_name=firstname,last_name=lastname)
+            return redirect('djangoapp:index')
+    return 0
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
